998c2df576d944677347f7ae32d9fcd786204914
rogelioRivas <apteruno@gmail.com>
2023-08-10 12:41:52 -0600

    wip

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const PORT = process.env.PORT || 3030
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
+
+app.get("/api/whoami", (req, res) => {
+  const response = {
+    ipaddress: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+    language: req.get("accept-language"),
+    software: req.get("user-agent"),
+  }
+  res.status(200).send(response)
+})
