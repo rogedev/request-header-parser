ef4810b27187d73f02cc54703a32778e74917943
RogelioRivas <apteruno@gmail.com>
2023-11-05 01:15:09 -0600

    add better format

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use("/public", express.static(__dirname + "/public"))
 
 const PORT = process.env.PORT || 3030
 
